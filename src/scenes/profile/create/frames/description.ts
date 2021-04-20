8a9a867566e706fd1d0ec5ff1255c3056cedcaf7
Soiran <toffuyume@gmail.com>
2021-04-20 23:08:16 +0300

    Supplemented

--- src/scenes/profile/create/frames/description.ts
+++ src/scenes/profile/create/frames/description.ts
@@ -8,23 +8,33 @@ import { Response } from './../../../../typings/global';
 
 export default new Frame(
     async (scene, options) => {
+        let keyboard = Keyboard.builder().textButton({
+            label: 'Не добавлять описание',
+            payload: {
+               withoutDescription: true 
+            },
+            color: Keyboard.SECONDARY_COLOR
+        }).oneTime();
         bot.sendMessage({
             message: options?.phrase || 'Расскажи о себе и своих интересах. Хорошее описание поможет найти подходящих тебе людей.',
             peer_id: scene.user.id,
-            ...scene.payload?.description && { keyboard: Keyboard.builder().textButton({
+            keyboard: scene.payload?.description ? keyboard.textButton({
                 label: 'Оставить текущее',
                 payload: {
                    leaveCurrent: true 
                 },
                 color: Keyboard.SECONDARY_COLOR
-            }).oneTime() }
+            }) : keyboard
         });
     },
     (message, scene) => {
         let description = message.text;
-        let leaveCurrent = message.messagePayload;
+        let payload = message.messagePayload;
         let response = descriptionValidator(description);
-        if (leaveCurrent) {
+        if (payload) {
+            if (payload?.withoutDescription) {
+                scene.payload.description = null;
+            }
             scene.next();
             return;
         }
