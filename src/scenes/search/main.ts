b3dcddf2acef7ac7bf33948c08ff34ae7411d3ea
Soiran <toffuyume@gmail.com>
2021-04-27 23:58:11 +0300

    Added reporting, small fix

--- src/scenes/search/main.ts
+++ src/scenes/search/main.ts
@@ -8,12 +8,27 @@ import Scene from '../../models/scene';
 import { ProfileView } from '../../typings/global';
 import MenuScene from '../menu';
 import MessageScene from './message';
+import ReportScene from './report';
 
 
 export default function SearchMainScene(payload?) {
     return new Scene('SearchMain', payload).add(new Frame(
         async scene => {
             let controller = scene.user;
+
+            if (!controller.canSearch) {
+                bot.sendMessage({
+                    peer_id: controller.id,
+                    message: 'Вы лайкнули достаточное количество людей за эти 24 часа, думаю вам стоит приостановиться.',
+                    keyboard: Keyboard.builder()
+                    .textButton({
+                        label: '😴',
+                        payload: { menu: true },
+                        color: Keyboard.SECONDARY_COLOR
+                    })
+                });
+            }
+
             let profile = scene.payload;
             let searchResult = await controller.search();
 
@@ -21,10 +36,11 @@ export default function SearchMainScene(payload?) {
                 bot.sendMessage({
                     peer_id: controller.id,
                     message: 'Упс, анкеты закончились! Напиши позже, чтобы найти новых людей.',
-                    keyboard: Keyboard.builder().textButton({
-                        label: 'Посмотреть новые анкеты',
-                        payload: { retry: true },
-                        color: Keyboard.POSITIVE_COLOR
+                    keyboard: Keyboard.builder()
+                    .textButton({
+                        label: '😴',
+                        payload: { menu: true },
+                        color: Keyboard.SECONDARY_COLOR
                     })
                 });
             } else {
@@ -32,15 +48,16 @@ export default function SearchMainScene(payload?) {
                     label: 'Продолжить',
                     color: Keyboard.POSITIVE_COLOR
                 });
-                let topText: string;
+                let topText: string = '';
+                let bottomText: string = '';
                 if (searchResult.type === ProfileView.STRANGER) {
-                    topText = '';
                     keyboard = Keyboard.builder()
                     .textButton({
                         label: '❤',
                         payload: { like: true },
                         color: Keyboard.POSITIVE_COLOR
-                    }).textButton({
+                    })
+                    .textButton({
                         label: '✍🏻',
                         payload: { message: true },
                         color: Keyboard.POSITIVE_COLOR
@@ -49,10 +66,15 @@ export default function SearchMainScene(payload?) {
                         label: '👎🏻',
                         payload: { dislike: true },
                         color: Keyboard.NEGATIVE_COLOR
-                    })
+                    }).row()
                     .textButton({
                         label: '😴',
                         payload: { menu: true },
+                        color: Keyboard.SECONDARY_COLOR
+                    })
+                    .textButton({
+                        label: '⚠️',
+                        payload: { report: true },
                         color: Keyboard.SECONDARY_COLOR
                     });
                 } else if (searchResult.type === ProfileView.LIKED) {
@@ -67,29 +89,36 @@ export default function SearchMainScene(payload?) {
                         label: '👎🏻',
                         payload: { dislike: true },
                         color: Keyboard.NEGATIVE_COLOR
-                    })
+                    }).row()
                     .textButton({
                         label: '😴',
                         payload: { menu: true },
                         color: Keyboard.SECONDARY_COLOR
+                    })
+                    .textButton({
+                        label: '⚠️',
+                        payload: { report: true },
+                        color: Keyboard.NEGATIVE_COLOR
                     });
+                    if (searchResult.message) bottomText = `\n\n✉️: ${searchResult.message}`;
                 } else if (searchResult.type === ProfileView.MUTUAL) {
                     topText = `Добавляйся в друзья - vk.com/id${searchResult.controller.id}\nУдачи вам провести время ;)\n\n`;
                 } else if (searchResult.type === ProfileView.REPORT) {
                     topText = 'Этот человек пожаловался на твою анкету:\n\n';
+                    bottomText = `\n\n⚠️: ${searchResult.message}`;
                 }
 
                 let foundProfile = await searchResult.controller.profile.data();
-                let distance = profile.latitude && foundProfile.latitude ? calculateDistance(
+                let distance = profile.latitude && foundProfile.latitude ? +calculateDistance(
                     profile.latitude,
                     profile.longitude,
                     foundProfile.latitude,
                     foundProfile.longitude
-                ) : 0;
+                ).toFixed(0) : 0;
                 let render = await searchResult.controller.profile.render(controller.profile, distance);
                 bot.sendMessage({
                     peer_id: controller.id,
-                    message: topText + render.text,
+                    message: topText + render.text + bottomText,
                     keyboard: keyboard,
                     attachment: render.photo
                 })
@@ -99,15 +128,16 @@ export default function SearchMainScene(payload?) {
         },
         async (message, scene) => {
             let payload = message.messagePayload;
-            if (payload?.retry) {
-                scene.retry();
-                return;
-            }
             let target: User = scene.payload.searchResult.controller;
             if (payload?.like) {
+                scene.retry();
                 scene.user.pick(target);
+                return;
             } else if (payload?.message) {
                 scene.user.setScene(MessageScene({ found: target }));
+                return;
+            } else if (payload?.report) {
+                scene.user.setScene(ReportScene({ found: target }));
                 return;
             } else if (payload?.menu) {
                 scene.user.setScene(MenuScene(scene.payload));
